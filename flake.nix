9a15565e3628a1ca74e4469b834eea3c36e21b34
Hakula Chen <i@hakula.xyz>
2026-05-10 00:46:27 +0800

    chore(deps): bump kiln to v0.3.0-rc.1
    Required for the named menu groups feature (`[[menu.social]]`) this PR
    relies on. v0.2.0 only supports the single `[menu]` table.

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 
     # kiln + pagefind (the kiln flake exposes both as `packages.${system}.*`).
     kiln = {
-      url = "github:hakula139/kiln/v0.2.0";
+      url = "github:hakula139/kiln/v0.3.0-rc.1";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
